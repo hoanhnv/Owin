ca2472f84796324bb3299cc32180e87c7eb42a85
swimtver <swim@live.ru>
2013-08-05 21:32:04 +0400

    create solution

---
+++ Owin.Web/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Owin.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseHandlerAsync((req, res) =>
+            {
+                res.ContentType = "text/plain";
+                return res.WriteAsync("OWIN Web Application");
+            });
+        }
+    }
+}
