378ecc4d153ede9a352ace0f4119aae2cd4d493f
swimtver <swim@live.ru>
2013-08-05 21:43:18 +0400

    Unit tests projects

---
+++ Owin.Web.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+
+namespace Owin.Web.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            new object().ShouldNotBeNull();
+        }
+    }
+}
