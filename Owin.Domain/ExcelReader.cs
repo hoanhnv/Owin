69a3a56d5eb9cee0de88b67eaea09647597309b8
Sergey Shuvalov <swim@live.ru>
2013-10-19 15:57:32 +0400

    new release

--- Owin.Domain/ExcelReader.cs
+++ Owin.Domain/ExcelReader.cs
@@ -4,20 +4,25 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinqToExcel;
 using Owin.Domain.Entities;
 
 namespace Owin.Domain
 {
     public class ExcelReader:ISourceReader
     {
-        public IEnumerable<Product> GetProductsFromStream(Stream stream)
+        public IEnumerable<Product> GetProductsFromStream(string filename)
         {
+            var excel = new ExcelQueryFactory(filename);
+
+            var n=excel.FileName;
+
             return Enumerable.Empty<Product>();
         }
     }
 
     public interface ISourceReader
     {
-        IEnumerable<Product> GetProductsFromStream(Stream stream);
+        IEnumerable<Product> GetProductsFromStream(string filename);
     }
 }
